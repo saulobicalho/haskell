572effe907abe73f5cb0774cbdbcbf633089ea0b
saulogrund <sbicalho86@gmail.com>
2019-02-21 21:49:10 -0300

    teste module, uses funcao1 wich uses fst,scd

--- haskell-tut.hs
+++ haskell-tut.hs
@@ -1,4 +1,13 @@
-import Data.List
-import System IO
+module Teste where
+funcao1 (a,b) (c,d) = ((+)(fst(a,b))(fst(c,d)),(*)(snd(a,b))(snd(c,d)))
 
-addMe :: Int-> Int-> Int
+data Pessoa = Pessoa {nome::Nome, idade::Idade, id::RG}
+type Nome = String
+type Idade = Integer
+type RG = String
+
+somaIdade :: (Pessoa -> Idade) -> Integer -> Idade
+somaIdade pessoaA x = (idade pessoaA) + x
+
+somaIdades :: [Pessoa] -> Idade
+somaIdades = foldr somaIdade 0
