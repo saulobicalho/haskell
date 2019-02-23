5415da7462556d297e3bbd2693328ebb9129e8fd
saulogrund <sbicalho86@gmail.com>
2019-02-23 19:26:30 -0300

    ex3

--- haskell-tut.hs
+++ haskell-tut.hs
@@ -1,3 +1,6 @@
+{--ex lista2 - cap4.4-1a 6 menos 5--}
+
+
 module Teste where
 
 data Pessoa = Pessoa {nome::Nome, idade::Idade, id::RG}
@@ -5,7 +8,15 @@ type Nome = String
 type Idade = Integer
 type RG = String
 
+{--ex2
 
 achaIdade p = idade p
 
 somaIdades = foldr ((+).(achaIdade)) 0
+--}
+
+--{--ex3
+
+maisNovo = foldr (min . idade) 999
+
+--}
