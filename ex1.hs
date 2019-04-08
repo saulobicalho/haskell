c9543ea5227e7a525a39f5c827cbc0c6a2b301b7
saulogrund <sbicalho86@gmail.com>
2019-04-08 15:37:18 -0300

    try 6

--- ex1.hs
+++ ex1.hs
@@ -27,6 +27,20 @@ break p = span (not.p)
 
 {--Questao 5--}
 
+splitWith:: (a->Bool) -> [a] -> [[a]]
+splitWith p = splitWith' p . dropWhile p
+splitWith' p [] = []
+splitWith' p x = x1: splitWith p x2
+ where (x1,x2) = break p x
 
 words:: String -> [String]
 words = splitWith isSpace
+
+{--Questao 6--}
+
+contaOcs:: [String] -> [(Int,String)]
+contaOcs [] = []
+contaOcs [x] = [(1,x)]
+contaOcs (x1:x2:xs)
+     | x1 == x2, ((yi, yv):ys) <- contaOcs (x2:xs) = (yi+1, yv) : ys
+     | otherwise = (1, x1) : contaOcs (x2:xs)
