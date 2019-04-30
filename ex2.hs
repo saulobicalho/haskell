8b99e218141445c49723c4a8ae6b16dd1da3d90d
unknown <sbicalho86@gmail.com>
2019-04-30 15:01:10 -0300

    complete

--- ex2.hs
+++ ex2.hs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Char
 import Data.List
 
---Questao 1 OK
+--Questao 1
 
 tresPrimeirasLetras:: [String] -> String
 tresPrimeirasLetras = foldr ((++).tomaTres) []
@@ -13,17 +13,12 @@ tresPrimeirasLetras = foldr ((++).tomaTres) []
 tomaTres::  String -> String
 tomaTres = take 3
 
---Questao 2 OK
+--Questao 2
 
 data Pessoa = Pessoa {nome::Nome, idade::Idade, identidade::RG} deriving (Eq)
 type Nome   = String
 type Idade  = Integer
 type RG     = String
-
-maria = Pessoa {nome = "Maria", idade= 32, identidade= "083"}
-carla = Pessoa {nome = "Carla", idade= 31, identidade= "082"}
-alice = Pessoa {nome = "Alice", idade= 37, identidade= "081"}
-grupo = [maria,carla,alice]
 
 somaIdades :: [Pessoa] -> Idade
 somaIdades = foldl  (flip((+).idade)) 0
@@ -35,7 +30,7 @@ instance Ord Pessoa where
 pessoaMaisNova :: [Pessoa] -> Nome
 pessoaMaisNova (p:ps)= nome $ foldr min p ps
 
---Questao 4 OK
+--Questao 4
 
 fazLista:: [String] ->[Char]
 fazLista = foldr ((++).processaString) []
@@ -46,8 +41,16 @@ processaInicioString = map toUpper.filter isAlpha.take 3
 processaString:: String ->String
 processaString a = processaInicioString a ++ drop 3 a
 
---Questao 5
---Questao 6 OK
+{--Questao 5
+
+Segue da definição da função foldr que foldr f z (a:x) = f a (foldr f z x).
+Dessa forma, tem-se que f a é lazy, ou seja, adota uma estratégia de avaliação
+preguiçosa. Sendo assim, se o argumento "a" for suficiente para determinar o
+resultado de f a (foldr f z x), não é necessário construir a computação para o
+valor do resto da lista (representado aqui por x).
+--}
+
+--Questao 6
 
 remDups :: Eq a => [a] -> [a]
 remDups = foldr (verificaDup) []
